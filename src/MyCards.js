00c7749f553c06a5ce9607f6625d6f5df080770e
Tyler K <tkcota@gmail.com>
2022-05-26 14:31:22 -0600

    update

---
+++ src/MyCards.js
@@ -0,0 +1,17 @@
+
+const initialCards = [
+  { id: 1, question: 'What is 2 + 2', answer: '4', correct: true },
+  { id: 2, question: 'What is 3+3', answer: '6', correct: true },
+  { id: 3, question: 'What is 5 + 5', answer: '10', correct: true },
+]
+
+const Cards = () => {
+  return (
+    <div>
+      <h1>{ }</h1>
+    </div>
+  )
+}
+
+
+export default Cards
